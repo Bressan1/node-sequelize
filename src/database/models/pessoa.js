b0b71a7eb2cc72f6cd95afbf7ecf2552b9dacf24
Bressan1 <edu.nick7@gmail.com>
2025-10-16 12:40:13 -0300

    creat function for validate cpf and scops for email and name, w msg personaliz

--- src/database/models/pessoa.js
+++ src/database/models/pessoa.js
@@ -1,4 +1,6 @@
 'use strict';
+const isCpfValido = require('../../utils/validaCpfHelper.js');
+
 const {
   Model
 } = require('sequelize');
@@ -19,9 +21,32 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   Pessoa.init({
-    nome: DataTypes.STRING,
-    email: DataTypes.STRING,
-    cpf: DataTypes.STRING,
+    nome: {
+    type: DataTypes.STRING,
+    validate:{
+      len:{
+        args: [3, 30],
+        msg: 'O nome deve ter entre 3 e 30 caracteres'
+      }
+    }
+  },
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: {
+          args: true,
+          msg: 'Email inválido'
+        }
+      }
+    },
+    cpf: {
+      type:DataTypes.STRING,
+      validate: {
+        cpfEhValido: (cpf ) => {
+          if (!isCpfValido(cpf)) throw new Error('CPF inválido');
+        }
+      }
+    },
     ativo: DataTypes.BOOLEAN,
     role: DataTypes.STRING
   }, {
